4ce5faff205a9d3d8c533af21d3e6cbff4f7fb02
Nandhini <nandhini@shiftx.ai>
2023-09-06 10:18:55 +0530

    Initial Commit

---
+++ Test_Git/src/Git_package/Test.java
@@ -0,0 +1,15 @@
+package Git_package;
+class ArthimeticOperation{
+	public int add(int a,int b){
+		System.out.println("addition");
+		return a+b;
+		
+	}
+}
+public class Test {
+	public static void main(String args[]) {
+		ArthimeticOperation addObj= new ArthimeticOperation();
+		addObj.add(10,20);
+		
+	}
+}
